4904eb8b5daa18c8887dd5a539dc3c1f3fb11486
cool_otaku <ericspz531@gmail.com>
2025-01-26 18:46:56 +0200

    Restore secret data

---
+++ app/src/main/java/com/example/online_victorine_app/Obj/Consts.java
@@ -0,0 +1,8 @@
+package com.example.online_victorine_app.Obj;
+
+public class Consts {
+
+    public static final String API_URL = "[YOU_API_URL]";
+    public static final String API_KEY = "[YOU_API_KEY]";
+
+}
